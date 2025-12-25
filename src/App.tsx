4077113a30fe5baa0fb1565cebaafd95b42a2130
oyajun <oyamadajun@outlook.jp>
2025-12-25 17:25:49 +0900

    tabelog

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,13 @@ function App() {
         <SearchForm onCriteriaChange={handleCriteriaChange} />
         <SearchResults results={results} helperMessage={helperMessage} />
       </main>
+      <footer className="affiliate-footer">
+        <div className="affiliate-banner">
+          <a href="//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3760146&pid=892412430" rel="nofollow">
+            <img src="//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=3760146&pid=892412430" border="0" />
+          </a>
+        </div>
+      </footer>
     </div>
   )
 }
